d4dbf3a42b3c25ab4adc13a03f71edaf0ff87e80
kynq <devquocky@gmail.com>
2024-02-23 00:47:01 +0700

    feat: implement ui for Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
-const Home = () =>{
-  return <div>page</div>;
-};
+import AddTodo from '@/components/shared/AddTodo'
 
-export default Home;
+const Home = () => {
+  return (
+    <div className="w-sceen py-20 flex justify-center flex-col items-center">
+      <span
+        className="text-3xl font-extrabold
+      uppercase"
+      >
+        TO-DO-APP
+      </span>
+      <h1
+        className="text-3xl font-extrabold 
+        uppercase mb-5"
+      >
+        Next.js 14
+        <span className="text-orange-700"> By Flag Nguyen</span>
+      </h1>
+
+      <div
+        className="fkex justify-center flex-col 
+        items-center w-[1000px]"
+      >
+        <AddTodo />
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- components/ui/Form.tsx
+++ components/ui/Form.tsx
@@ -1,5 +1,28 @@
-const Home = () =>{
-    return <div>page</div>;
-  };
-  
-  export default Home;
+'use client'
+import { ReactNode, useRef } from 'react'
+
+interface formProps {
+  children: ReactNode
+  action: (FormData: FormData) => Promise<void | boolean>
+  className?: string
+  onSubmit?: () => void
+}
+
+const Form = ({ children, action, className, onSubmit }: formProps) => {
+  const ref = useRef<HTMLFormElement>(null)
+  return (
+    <form
+      className={className}
+      onSubmit={onSubmit}
+      ref={ref}
+      action={async (formData) => {
+        await action(formData)
+        ref.current?.reset()
+      }}
+    >
+      {children}
+    </form>
+  )
+}
+
+export default Form
